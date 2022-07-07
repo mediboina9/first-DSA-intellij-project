273f8fdd859421d0afe09c8c799fb9c96c319005
siva <mediboyanasiva@gmaiil.com>
2022-07-07 15:11:14 +0530

    sum value Ascending order

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -40,7 +40,7 @@ public class Main {
 
       // System.out.println(fib(60));
         FibonaaciSeries fibonaaciSeries=new FibonaaciSeries();
-        fibonaaciSeries.Fibancci1000SeriesUsingArray(1001);
+        fibonaaciSeries.Fibancci1000SeriesUsingArray(1000);
 
     }
 
