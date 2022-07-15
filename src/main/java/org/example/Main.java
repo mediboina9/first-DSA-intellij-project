88e6bce426cb9e6604005712112dd49647233d83
siva <mediboyanasiva@gmaiil.com>
2022-07-15 21:00:01 +0530

    groupingBy SchoolName using java8 steams

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 
 import org.example.amirGLInterview.FibonaaciSeries;
+import org.example.grouping.Student;
+import org.example.grouping.StudentSchoolName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static int fib(int n){
@@ -39,8 +46,22 @@ public class Main {
         System.out.println(str1.equals(str2));*/
 
       // System.out.println(fib(60));
-        FibonaaciSeries fibonaaciSeries=new FibonaaciSeries();
-        fibonaaciSeries.Fibancci1000SeriesUsingArray(1000);
+       // FibonaaciSeries fibonaaciSeries=new FibonaaciSeries();
+       // fibonaaciSeries.Fibancci1000SeriesUsingArray(1000);
+
+        ArrayList<Student> list=new ArrayList<>(){
+            {
+                add(new Student(1,"siva",6,"zpph", "kakinada"));
+                        add(new Student(2,"krisha",8,"zpph", "kakinada"));
+                        add(new Student(3,"Devi",8,"zpph", "kakinada"));
+                        add(new Student(4,"shankar",1,"basham", "kakinada"));
+                        add(new Student(5,"sandeep",6,"basham", "thanuku"));
+            }
+        };
+
+        Map<String, List<Student>> groupBySchoolName=list.stream().collect(Collectors.groupingBy(Student::getSchoolName));
+        groupBySchoolName.entrySet().stream().forEach(System.out::println);
+
 
     }
 
