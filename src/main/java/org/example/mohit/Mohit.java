9cbf7fe0989eb890c135f7876ade5e380391c5ee
siva <mediboyanasiva@gmaiil.com>
2022-07-29 11:34:30 +0530

    hashMap elements sorted by using AL and LinkedHashMap.

--- src/main/java/org/example/mohit/Mohit.java
+++ src/main/java/org/example/mohit/Mohit.java
@@ -1,102 +1,92 @@
 package org.example.mohit;
 
 import java.io.*;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Mohit {
     public static void main(String[] args) throws IOException {
         HashMap<Student,Subject> hashMap=new HashMap<>();
-        File file=new File("/Users/siva/Downloads/","file.docx");
-        //FileReader fr=new FileReader()
-        System.out.println("File: "+file.getName());
-        System.out.println("File abs path: "+file.getAbsolutePath());
-        System.out.println("File parent: "+file.getParent());
-        System.out.println("File total space: "+file.getTotalSpace());
-        System.out.println("File abs file: "+file.getAbsoluteFile());
-        System.out.println("File toString: "+file.toString());
-        System.out.println("File length: "+file.length());
-        System.out.println("File can read: "+file.canRead());
-        System.out.println("File last modifiled: "+file.lastModified());
-        String dir="Users/siva/Downloads/";
-        File fileDir=new File(dir);
-        System.out.println(fileDir.getName()+" downloads dir name");
-        System.out.println(fileDir.isDirectory()+" dowloads is directory");
-        int size;
-        try (FileInputStream fip=new FileInputStream("/Users/siva/Downloads/file.docx")){
-            System.out.println("Total Available Bytes: " + (size = fip.available()));
-            int n=size/40;
-            for(int i=0;i<n;i++){
-                System.out.print((char) fip.read());
-            }
-            System.out.println("\nStill Available: " + fip.available());
-            System.out.println("Reading the next " + n +
-                    " with one read(b[])");
-            byte b[] = new byte[n];
-            if (fip.read(b) != n) {
-                System.err.println("couldn’t read " + n + " bytes.");
-            }
-            System.out.println(new String(b, 0, n));
-            System.out.println("\nStill Available: " + (size = fip.available()));
+       Student student=new Student(1,"siva");
+       Student student1=new Student(2,"Narayana");
+       Student student2=new Student(3, "Krishna");
+
+       Subject subject=new Subject(1,"Mathematics");
+       Subject subject1=new Subject(2,"Physics");
+       Subject subject2=new Subject(3, "Chemistry");
+
+       hashMap.put(student,subject);
+       hashMap.put(student1,subject1);
+       hashMap.put(student2,subject);
+       System.out.println("hashMap index: "+hashMap.get(student));
+       ArrayList<Student> al=new ArrayList<>();
+       LinkedHashMap<Student,Subject> linkedHashMap=new LinkedHashMap<>();
+        for(Student i:hashMap.keySet()){
+            System.out.println(i);
+            al.add(i);
         }
-        String source = "Now is the time for all good men\n"
-                + " to come to the aid of their country\n"
-                + " and pay their due taxes.";
-        byte buf[] = source.getBytes();
-        System.out.println(source);
-        System.out.println((char) buf[0]);
-        FileOutputStream f0 = null;
-        try {
-            f0 = new FileOutputStream("/Users/siva/Downloads/FILE_OUTPUT.rtf");
-
-            // write to first file
-            for (int i=0; i < buf.length; i ++) f0.write(buf[i]);
-            // write to second file
-           
-        } catch(IOException e) {
-            System.out.println("An I/O Error Occurred");
+        Collections.sort(al);
+       // al.stream().sorted(Comparator.comparing(Student::getStudentName)).map(i->linkedHashMap.put(hashMap.get(i)));
+        for (Student i:al){
+            linkedHashMap.put(i,hashMap.get(i));
         }
-        FileReader fileReader=new FileReader("/Users/siva/Downloads/FILE_OUTPUT.rtf");
-        BufferedReader bufferedReader=new BufferedReader(fileReader);
-        System.out.println(bufferedReader.readLine());
-        System.out.println(bufferedReader.readLine());
-        System.out.println(bufferedReader.readLine());
-        int i;
-        while((i=bufferedReader.read())!=-1){
-            System.out.print((char)i);
-        }
-        bufferedReader.close();
-        fileReader.close();
-
-        FileInputStream fileInputStream=new FileInputStream("/Users/siva/Downloads/FILE_OUTPUT.rtf");
-        System.out.println((char) fileInputStream.read());
-        System.out.println("Total Available Bytes: " +
-                (size = fileInputStream.available()));
-        int n=size/3;
-        for(int j=0;j<n;j++){
-            System.out.print((char) fileInputStream.read());
-        }
-        byte b[] = new byte[n];
-        if (fileInputStream.read(b) != n) {
-            System.err.println("couldn’t read " + n + " bytes.");
-        }
-        System.out.println(new String(b, 0, n));
-       // System.out.println(new String(f, 0, n));
-
-
-
-
-
-        //System.out.println(fip.available());
-
+        linkedHashMap.entrySet().stream().forEach(System.out::println);
     }
 
 }
-class Student {
+
+class Student implements Comparable<Student> {
     int studentId;
     String studentName;
 
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(studentName.compareTo(o.studentName)==0) {
+            return 0;
+        }
+
+            else if (studentName.compareTo(o.studentName)==1){
+                return 1;
+            }
+            else return -1;
+
+    }
 }
 class Subject{
     int id;
     String subName;
+
+    public Subject(int id, String subName) {
+        this.id = id;
+        this.subName = subName;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "id=" + id +
+                ", subName='" + subName + '\'' +
+                '}';
+    }
 }
