db086f77b88cee62e80637d6dfd2b36f71f47da2
siva <mediboyanasiva@gmaiil.com>
2022-08-05 16:03:11 +0530

    separating every interview have main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -37,17 +37,7 @@ public class Main {
         //BST Tree Data
        // DataDefine o=new DataDefine();
 
-     /*   IAdd obj=x -> {return x+x;};
-        System.out.println(obj.sum(10));
 
-        String str1=new String("abc");
-        String str2=new String("abc");
-        System.out.println(str1==str2);
-        System.out.println(str1.equals(str2));*/
-
-      // System.out.println(fib(60));
-       // FibonaaciSeries fibonaaciSeries=new FibonaaciSeries();
-       // fibonaaciSeries.Fibancci1000SeriesUsingArray(1000);
 
         ArrayList<Student> list=new ArrayList<>(){
             {
--- src/main/java/org/example/amirGLInterview/LambadExpressonAndStrings.java
+++ src/main/java/org/example/amirGLInterview/LambadExpressonAndStrings.java
@@ -1,5 +1,16 @@
 package org.example.amirGLInterview;
 
 public class LambadExpressonAndStrings {
+    public static void main(String[] args) {
+            IAdd obj=x -> {return x+x;};
+        System.out.println(obj.sum(10));
 
+        String str1=new String("abc");
+        String str2=new String("abc");
+        System.out.println(str1==str2);
+        System.out.println(str1.equals(str2));
+
+         FibonaaciSeries fibonaaciSeries=new FibonaaciSeries();
+         fibonaaciSeries.Fibancci1000SeriesUsingArray(1000);
+    }
 }
